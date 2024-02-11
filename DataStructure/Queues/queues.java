1c4987442960a7bcec5b477e1826fbd7884dde00
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-11 23:45:37 +0530

    add file

--- DataStructure/Queues/queues.java
+++ DataStructure/Queues/queues.java
@@ -7,6 +7,9 @@ public class queues {
         System.out.println("hi from queue");
         System.out.println("hi from queue");
         System.out.println("hi from queue");
-        
+        System.out.println("hi from queue");
+        System.out.println("hi from queue");
+        System.out.println("hi from queue");
+
     }
 }
