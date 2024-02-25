0329261ca2e22862803736cdd301bdbdeca68a69
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-25 23:43:36 +0530

    add file

---
+++ DataStructure/String/TEst.java
@@ -0,0 +1,10 @@
+package DataStructure.String;
+
+public class TEst {
+    public static void main(String[] args) {
+//        System.out.println(30+20);
+//        int roll=25;
+//        System.out.println(roll.types);
+    }
+
+}
