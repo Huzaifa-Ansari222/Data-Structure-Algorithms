ffc4a82f37bda43925920af967819a203f6d1607
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-28 04:29:06 +0530

    addfile

--- DataStructure/Array_2D/Jagged_2D_array.java
+++ DataStructure/Array_2D/Jagged_2D_array.java
@@ -1,2 +1,24 @@
-package DataStructure.Array_2D;public class Jagged_2D_array {
+//unequal row length called jagged
+package DataStructure.Array_2D;
+
+public class Jagged_2D_array {
+    public static void main(String[] args) {
+        //    int [] [] arr = new int[] [4]; //not allow
+        int [] [] arr = new int[3] []; //allow
+        arr[0] = new int[2]; // 1row 2col
+        arr[1] = new int [5];// 2row 5col
+        arr[2] = new int [1];// 3row 1col
+
+        System.out.println(arr[0].length);
+        System.out.println(arr[1].length);
+        System.out.println(arr[2].length);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) { //go till i row len
+                System.out.print("A["+i+"]["+j+"] =>" +arr[i][j]+"  ");
+            }
+            System.out.println("");
+        }
+
+    }
 }
