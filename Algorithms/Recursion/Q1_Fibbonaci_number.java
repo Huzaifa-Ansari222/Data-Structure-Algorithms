1a07f873e57ccf8aec7b15f9c37989cae9ff4b53
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-03-17 23:58:33 +0530

    add file

--- Algorithms/Recursion/Q1_Fibbonaci_number.java
+++ Algorithms/Recursion/Q1_Fibbonaci_number.java
@@ -2,13 +2,13 @@ package Algorithms.Recursion;
 
 public class Q1_Fibbonaci_number {
     public static void main(String[] args) {
-        System.out.println(   fibo(7) );
+        System.out.println(   fibo(4) );
     }
     static int fibo(int n){
         if (n<2){ //0-> 0 ; 1-> 1 ;
             return n;
         }
-        return fibo(n-2) + fibo(n-1);
+        return fibo(n-1) + fibo(n-2);
 
     }
 }
