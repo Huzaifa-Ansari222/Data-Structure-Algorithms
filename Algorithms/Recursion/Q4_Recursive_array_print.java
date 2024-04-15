a0b11e23994896c6ca60845c340a50a2cfdbb87b
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-04-15 08:52:15 +0530

    add files

---
+++ Algorithms/Recursion/Q4_Recursive_array_print.java
@@ -0,0 +1,19 @@
+package Algorithms.Recursion;
+
+public class Q4_Recursive_array_print {
+    public static void main(String[] args) {
+        int [] arr = {11,22,33,44};
+        printArray(arr , 0);
+    }
+    static void printArray(int []arr, int idx){
+        // base case
+        if(idx >= arr.length) return; //0>=6
+        System.out.println("arr: "+arr.length+ " idx: " +idx);
+        //self work
+        System.out.println(arr[idx]);
+
+        //recusive call
+        printArray(arr, idx +1);
+
+    }
+}
