0116792929ac620833f39c4fe6a0e97a29b50e04
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-10-29 00:53:18 +0530

    added code file

--- Algorithms/Recursion/Q2_Factorial_number.java
+++ Algorithms/Recursion/Q2_Factorial_number.java
@@ -2,12 +2,12 @@ package Algorithms.Recursion;
 
 public class Q2_Factorial_number {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 2;
         System.out.println(factor(n));
     }
     static int factor(int n){
         //base case
-        if( n == 2) {
+        if( n == 1 || n == 0) {
             return 1;
         }
         return n * factor(n-1);
--- Algorithms/Recursion/Q3_PowerOF.java
+++ Algorithms/Recursion/Q3_PowerOF.java
@@ -2,12 +2,12 @@ package Algorithms.Recursion;
 
 public class Q3_PowerOF {
     public static void main(String[] args) {
-        System.out.println( power(4,5));
+        System.out.println( power(2,3));
     }
     static  int  power (int a , int b ){
         if (b==0){
             return 1;
         }
-        return a * power(a, b-1);//a * (a^b-1)
+        return a * power(a, b-1);//a * (a, b-1)
     }
 }
